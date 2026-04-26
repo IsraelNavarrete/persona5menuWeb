539a56dfa20d4a2cf73ba45083280c8e83c89ee0
Israel <israelnavarreterodriguez@gmail.com>
2026-04-26 13:16:53 +0200

    Añadida "animacion" de seleccion de boton (No hacer la pagina mas pequeña o grande porque no funciona bien y no lo voy a solucionar probablemente)

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,4 +1,5 @@
 
+const animationVelocity = 50;
 
 window.onload = (event) => {
 
@@ -13,7 +14,7 @@ window.onload = (event) => {
 };
 
 
-function addEvent(buttons){
+function addEvent(buttons) {
 
     console.log("Lista de elementos para eventos: " + buttons);
 
@@ -25,12 +26,19 @@ function addEvent(buttons){
 
         element.addEventListener("mouseover", () => onMouseOverEvent(element));
         element.addEventListener("mouseleave", () => onMouseLeaveEvent(element));
+        element.addEventListener("click", () => onClickEvent(element));
 
     }
 
 }
 
-function onMouseOverEvent(element){
+function onClickEvent(element){
+
+    console.log("Pulsado " + element);
+
+}
+
+function onMouseOverEvent(element) {
 
     const red = element.children[1].children[0];
     const blue = element.children[1].children[1];
@@ -38,11 +46,15 @@ function onMouseOverEvent(element){
     red.setAttribute("visibility", "visible");
     blue.setAttribute("visibility", "visible");
 
+    if (!element.animation) {
+        element.animation = setInterval(() => animate([red, blue], element), animationVelocity);
+    }
+
     console.log("Visibility added");
 
 }
 
-function onMouseLeaveEvent(element){
+function onMouseLeaveEvent(element) {
 
     const red = element.children[1].children[0];
     const blue = element.children[1].children[1];
@@ -50,7 +62,36 @@ function onMouseLeaveEvent(element){
     red.setAttribute("visibility", "hidden");
     blue.setAttribute("visibility", "hidden");
 
+    clearInterval(element.animation);
+    element.animation = null;
+
     console.log("Visibility hidden");
+}
+
+function animate(polygons, svg) {
+
+    const horizontalCenterPadding = 20;
+    const verticalCenterPadding = 5;
+
+    const horizontalEdgePadding = 0;
+    const verticalEdgePadding = 0;
+
+    const centerX = svg.getBoundingClientRect().width / 2;
+    const centerY = svg.getBoundingClientRect().height / 2;
+    const w = centerX - horizontalCenterPadding - horizontalEdgePadding;
+    const h = centerY - verticalCenterPadding - verticalEdgePadding;
+
+    polygons.forEach(element => {
+        const newPoints = `
+        ${Math.random() * w + horizontalEdgePadding} ${Math.random() * h + verticalEdgePadding},
+        ${Math.random() * w + centerX + horizontalCenterPadding} ${Math.random() * h + verticalEdgePadding},
+        ${Math.random() * w + centerX + horizontalCenterPadding} ${Math.random() * h + centerY + verticalCenterPadding},
+        ${Math.random() * w + horizontalEdgePadding} ${Math.random() * h + centerY + verticalCenterPadding}
+    `;
+
+        element.setAttribute("points", newPoints);
+    });
+
 }
 
 
@@ -64,7 +105,7 @@ function addSVG(elements) {
 
         console.log(element);
 
-        const svg = document.createElementNS("http://www.w3.org/2000/svg","svg");
+        const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         const elementPosition = element.getBoundingClientRect();
 
         svg.setAttribute("class", element.className + "-svg");
@@ -83,8 +124,8 @@ function addSVG(elements) {
 
 function addPolygon(element, height, width) {
 
-    const red = document.createElementNS("http://www.w3.org/2000/svg","polygon");
-    const blue = document.createElementNS("http://www.w3.org/2000/svg","polygon");
+    const red = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+    const blue = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
 
     red.setAttribute("class", "red");
     blue.setAttribute("class", "blue");
